60521a6d1782a805ed5f8f035fda36e2cbee27a3
Frede-72 <frjo0005@stud.ek.dk>
2026-06-07 13:15:34 +0200

    test

--- src/test/java/ek/osnb/starter/repository/DataSourceTest.java
+++ src/test/java/ek/osnb/starter/repository/DataSourceTest.java
@@ -3,15 +3,13 @@ package ek.osnb.starter.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
-import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
 
 @DataJpaTest
 // Uncomment the following annotations to test with the Docker container database
-@ActiveProfiles("docker")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@ActiveProfiles("docker")
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DataSourceTest {
     @Autowired
     DataSource dataSource;
